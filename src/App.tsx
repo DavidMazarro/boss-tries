d6cc7e28a3c20076597d4735fb79b202993e65ef
DavidMazarro <davidmazarro98@gmail.com>
2022-04-26 12:56:23 +0200

    Basic project setup completed (hopefully)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import './styles.css'
+import "./styles.css";
+import ICON from "./React-icon.svg";
+import { Counter } from "./Counter";
+import Button from "react-bootstrap/Button";
 
 export const App = () => {
-  return <>
-  <h1>React Typescript Webpack Starter Template</h1>
-  </>
-}
+  return (
+    <>
+      <h1>
+        React Typescript Webpack Starter Template - {process.env.NODE_ENV}{" "}
+        {process.env.name}
+      </h1>
+      <img src={ICON} alt="React Logo" width="300" />
+      <br />
+      <Button variant="primary" className="mr-1">
+        CLICK ME!
+      </Button>
+      <Counter></Counter>
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import ReactDOM, { render } from "react-dom";
+import ReactDOM from "react-dom";
 import { App } from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
